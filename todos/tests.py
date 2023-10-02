4c2ed744c73be066122e8797405f483794cdd8ba
Walia8416 <adi.walia16@gmail.com>
2023-10-02 12:42:30 +0530

    ToDo's Backend

---
+++ todos/tests.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+from .models import ToDo
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+# Create your tests here.
+
+
+class ToDoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = ToDo.objects.create(
+            title='first',
+            body='body boy'
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title,"first")
+        self.assertEqual(self.todo.body,"body boy")
+        self.assertEqual(str(self.todo),"first")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse('todo_list'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(ToDo.objects.count(),1)
+        self.assertContains(response,self.todo)
+
+    def test_api_detailview(self):
+        response = self.client.get(reverse('todo_detail',kwargs={"pk":self.todo.id}),format="json")
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(ToDo.objects.count(),1)
+        self.assertContains(response,"first")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
